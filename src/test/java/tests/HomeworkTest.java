bf3ddf35a3677bcbdb54747922d1fb26846bfa73
Tatiana Kazakova <102759947+swastest@users.noreply.github.com>
2022-06-07 21:38:39 +0300

    Update HomeworkTest.java

--- src/test/java/tests/HomeworkTest.java
+++ src/test/java/tests/HomeworkTest.java
@@ -30,7 +30,14 @@ public class HomeworkTest {
     @Test
     void getListUsers2() {
 
-        String body = "{ page: 2, per_page: 6, total: 12, total_pages: 2, data: [ { id: 7, email: \"michael.lawson@reqres.in\", first_name: \"Michael\", last_name: \"Lawson\", avatar: \"https://reqres.in/img/faces/7-image.jpg\" }, { id: 8, email: \"lindsay.ferguson@reqres.in\", first_name: \"Lindsay\", last_name: \"Ferguson\", avatar: \"https://reqres.in/img/faces/8-image.jpg\" }, { id: 9, email: \"tobias.funke@reqres.in\", first_name: \"Tobias\", last_name: \"Funke\", avatar: \"https://reqres.in/img/faces/9-image.jpg\" }, { id: 10, email: \"byron.fields@reqres.in\", first_name: \"Byron\", last_name: \"Fields\", avatar: \"https://reqres.in/img/faces/10-image.jpg\" }, { id: 11, email: \"george.edwards@reqres.in\", first_name: \"George\", last_name: \"Edwards\", avatar: \"https://reqres.in/img/faces/11-image.jpg\" }, { id: 12, email: \"rachel.howell@reqres.in\", first_name: \"Rachel\", last_name: \"Howell\", avatar: \"https://reqres.in/img/faces/12-image.jpg\" } ], support: { url: \"https://reqres.in/#support-heading\", text: \"To keep ReqRes free, contributions towards server costs are appreciated!\" } }";
+        String body = "{ page: 2, per_page: 6, total: 12, total_pages: 2, data: [ { id: 7, email: \"michael.lawson@reqres.in\", 
+            first_name: \"Michael\", last_name: \"Lawson\", avatar: \"https://reqres.in/img/faces/7-image.jpg\" }, 
+            { id: 8, email: \"lindsay.ferguson@reqres.in\", first_name: \"Lindsay\", last_name: \"Ferguson\", avatar: \"https://reqres.in/img/faces/8-image.jpg\" }, 
+            { id: 9, email: \"tobias.funke@reqres.in\", first_name: \"Tobias\", last_name: \"Funke\", avatar: \"https://reqres.in/img/faces/9-image.jpg\" }, 
+            { id: 10, email: \"byron.fields@reqres.in\", first_name: \"Byron\", last_name: \"Fields\", avatar: \"https://reqres.in/img/faces/10-image.jpg\" }, 
+            { id: 11, email: \"george.edwards@reqres.in\", first_name: \"George\", last_name: \"Edwards\", avatar: \"https://reqres.in/img/faces/11-image.jpg\" }, 
+            { id: 12, email: \"rachel.howell@reqres.in\", first_name: \"Rachel\", last_name: \"Howell\", avatar: \"https://reqres.in/img/faces/12-image.jpg\" } ], 
+                support: { url: \"https://reqres.in/#support-heading\", text: \"To keep ReqRes free, contributions towards server costs are appreciated!\" } }";
         given()
                 .log().uri()
                 .log().body()
@@ -77,7 +84,14 @@ public class HomeworkTest {
 
     @Test
     void getListResource() {
-        String body = "{ page: 1, per_page: 6, total: 12, total_pages: 2, data: [ { id: 1, name: \"cerulean\", year: 2000, color: \"#98B2D1\", pantone_value: \"15-4020\" }, { id: 2, name: \"fuchsia rose\", year: 2001, color: \"#C74375\", pantone_value: \"17-2031\" }, { id: 3, name: \"true red\", year: 2002, color: \"#BF1932\", pantone_value: \"19-1664\" }, { id: 4, name: \"aqua sky\", year: 2003, color: \"#7BC4C4\", pantone_value: \"14-4811\" }, { id: 5, name: \"tigerlily\", year: 2004, color: \"#E2583E\", pantone_value: \"17-1456\" }, { id: 6, name: \"blue turquoise\", year: 2005, color: \"#53B0AE\", pantone_value: \"15-5217\" } ], support: { url: \"https://reqres.in/#support-heading\", text: \"To keep ReqRes free, contributions towards server costs are appreciated!\" } }";
+        String body = "{ page: 1, per_page: 6, total: 12, total_pages: 2, data: 
+            [ { id: 1, name: \"cerulean\", year: 2000, color: \"#98B2D1\", pantone_value: \"15-4020\" }, 
+            { id: 2, name: \"fuchsia rose\", year: 2001, color: \"#C74375\", pantone_value: \"17-2031\" }, 
+            { id: 3, name: \"true red\", year: 2002, color: \"#BF1932\", pantone_value: \"19-1664\" }, 
+            { id: 4, name: \"aqua sky\", year: 2003, color: \"#7BC4C4\", pantone_value: \"14-4811\" }, 
+            { id: 5, name: \"tigerlily\", year: 2004, color: \"#E2583E\", pantone_value: \"17-1456\" }, 
+            { id: 6, name: \"blue turquoise\", year: 2005, color: \"#53B0AE\", pantone_value: \"15-5217\" } ], 
+                support: { url: \"https://reqres.in/#support-heading\", text: \"To keep ReqRes free, contributions towards server costs are appreciated!\" } }";
         given()
                 .log().uri()
                 .log().body()
@@ -120,7 +134,7 @@ public class HomeworkTest {
                 .statusCode(200)
                 .body("name", is("morpheus"))
                 .body("job", is("zion resident"))
-                .body("updatedAt", is("2022-06-07T17:41:26.056Z")); // не поняла как решить. начение динамичное ?
+                .body("updatedAt", notNullValue()); // не поняла как решить. начение динамичное ? все ндинамические значения вот так notNullValue()   - просто что там не пусто
     }
 
     @Test
